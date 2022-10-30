721aaaa28a0ff293c3aaa450a6819f8fb7a7f812
TzuHsien-Yeh <TzuHsien.soft@gmail.com>
2022-10-30 12:22:45 +0800

    Play YouTube video in app

--- app/src/main/java/com/tzuhsien/immediat/takenote/TakeNoteFragment.kt
+++ app/src/main/java/com/tzuhsien/immediat/takenote/TakeNoteFragment.kt
@@ -1,13 +1,19 @@
 package com.tzuhsien.immediat.takenote
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import com.tzuhsien.immediat.databinding.FragmentTakeNoteBinding
 import com.tzuhsien.immediat.ext.getVmFactory
+
 
 class TakeNoteFragment : Fragment() {
 
@@ -22,6 +28,16 @@ class TakeNoteFragment : Fragment() {
     ): View? {
 
         binding = FragmentTakeNoteBinding.inflate(layoutInflater)
+
+        val youTubePlayerView: YouTubePlayerView = binding.youtubePlayerView
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
+                val videoId = viewModel.videoId
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
 
         return binding.root
     }
